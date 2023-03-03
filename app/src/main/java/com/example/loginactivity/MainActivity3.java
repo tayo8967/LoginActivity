d7391cc69183124754d95cfb52c01b0ab3421dd8
unknown <marcodiaz8967@gmail.com>
2023-03-04 00:12:23 +0800

    Added stop/resume button.

--- app/src/main/java/com/example/loginactivity/MainActivity3.java
+++ app/src/main/java/com/example/loginactivity/MainActivity3.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -14,7 +15,9 @@ import android.widget.VideoView;
 public class MainActivity3 extends AppCompatActivity {
 
     VideoView videoView;
-    Button logOutBtn;
+    Button logOutBtn, stopResumeBtn;
+    volatile boolean isPlaying = true;
+    Handler mainHandler = new Handler();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,6 +27,7 @@ public class MainActivity3 extends AppCompatActivity {
 
         videoView = findViewById(R.id.video_view);
         logOutBtn = findViewById(R.id.logOutBtn);
+        stopResumeBtn = findViewById(R.id.stopResumeBtn);
         String videoUri = "android.resource://" + getPackageName() + "/" + R.raw.video;
 
         VideoPlayback videoPlayback = new VideoPlayback(videoUri);
@@ -39,7 +43,42 @@ public class MainActivity3 extends AppCompatActivity {
                 finish();
             }
         });
+
+        stopResumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StopResume stopResume = new StopResume();
+                new Thread(stopResume).start();
+            }
+        });
     }
+
+    class StopResume implements Runnable{
+        @Override
+        public void run() {
+            if(isPlaying){
+                videoView.pause();
+                isPlaying = false;
+                mainHandler.post(new Runnable(){
+                    @Override
+                    public void run(){
+                        stopResumeBtn.setText("Resume");
+                    }
+                });
+            }
+            else{
+                videoView.start();
+                isPlaying = true;
+                mainHandler.post(new Runnable(){
+                   @Override
+                   public void run(){
+                       stopResumeBtn.setText("Stop");
+                   }
+                });
+            }
+        }
+    }
+
 
     class VideoPlayback implements Runnable{
         String videoUri;
